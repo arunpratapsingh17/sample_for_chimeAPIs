72e6cecfdc4682358bda16d9f83bf0b8ec20c50d
arunpratapsingh17 <218apsj@gmail.com>
2021-12-07 09:52:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { listChannelMemberships, listChannels } from './api/ChimeAPI';
+import {useEffect} from "react";
+import AWS from "aws-sdk"
+import appConfig from './Config';
+import { Auth } from '@aws-amplify/auth';
 function App() {
+  useEffect(()=>{
+    console.log(AWS.config);
+    //Authenticated credentials required for any API request.
+    const creds = await Auth.currentCredentials();
+    const essentialCreds = await Auth.essentialCredentials(creds);
+    AWS.config.credentials = essentialCreds;
+    AWS.config.region = appConfig.region;
+    console.log(AWS.config);
+  },[])
+  const onClickHandler=async()=>{
+    //Sample API run.
+    const res = await listChannels('arn:aws:chime:us-east-1:587273692666:app-instance/8bb40969-423c-4e01-906d-6298cacb6def',"us-east-1:aa34a01a-25ab-4112-b611-48f1b03f4375");
+    console.log(res);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hey</p>
+      <button onClick={onClickHandler}>
+        Click to run fetchPublicChatroomAPI
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,6 +11,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+//arn:aws:chime:us-east-1:587273692666:app-instance/8bb40969-423c-4e01-906d-6298cacb6def
+//us-east-1:aa34a01a-25ab-4112-b611-48f1b03f4375
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
